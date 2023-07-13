b7bd6e841496e8210de039a3a52f46ba5da50caa
afrazier01 <amfrazier01@gmail.com>
2023-07-13 15:56:24 -0500

    added login functionality

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,18 +2,21 @@ const router = require('express').Router();
 const { Post, User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req,res) => {
+router.get('/',withAuth , async (req,res) => {
     try {
         const postData = await Post.findAll({
-            include: [{model: User}]
+            include: [{
+                model: User,
+                attributes: {exclude: ['password']}
+            }]
         });
 
-        res.render('homepage',{
-            postData
-        })
-        
+      res.render('homepage',{
+        postData,
+        logged_in: req.session.logged_in,
+      })
     } catch (error) {
-
+        res.status(500).json(error)
     }
 });
 
