5a113541d59949185de16e100dc0d44106fbf654
afrazier01 <amfrazier01@gmail.com>
2023-07-16 04:32:31 -0500

    Fixed issue preventing stylesheet from being applied

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -29,24 +29,4 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// router.get('/', withAuth, async (req, res) => {
-//   try {
-//     const userData = await User.findAll({
-//       attributes: { exclude: ['password'] },
-//       order: [['name', 'ASC']],
-//     });
-
-//     const users = userData.map((Post) => Post.get({ plain: true }));
-
-//     res.render('homepage', {
-//       users,
-//       logged_in: req.session.logged_in,
-//     });
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
 module.exports = router;
